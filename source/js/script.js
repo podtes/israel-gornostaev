0d5ccc9ef17fffa99506e4a4aa658fda6c3a2a55
podtes <dpodtes@gmail.com>
2019-12-21 18:47:47 +0400

    реализовано открытие и закрытие модальных окон через JS, осталоась проблема с отправкой формы

--- source/js/script.js
+++ source/js/script.js
@@ -8,16 +8,21 @@ var callbackMeButton = callbackModal.querySelector('input[type="submit"]');
 var successModal = overlay.querySelector('.modal-success');
 var successModalOKButton = successModal.querySelector('button[type="button"]');
 var successModalCloseButton = successModal.querySelector('.modal-order-callback__close-button');
+var nameInput = callbackModal.querySelector('input[name="name"]');
+var form = callbackModal.querySelector('form');
 
 var closeCallbackModal = function () {
   overlay.classList.remove('modal-open');
   callbackModal.classList.remove('modal-open');
   document.removeEventListener('keydown', openCallbackModalPressEscHandler);
+  overlay.removeEventListener('click', overlayClickHandler);
 };
 var openCallbackModal = function () {
   overlay.classList.add('modal-open');
   callbackModal.classList.add('modal-open');
+  nameInput.focus();
   document.addEventListener('keydown', openCallbackModalPressEscHandler);
+  overlay.addEventListener('click', overlayClickHandler);
 };
 
 var openCallbackModalPressEscHandler = function (evt) {
@@ -46,12 +51,15 @@ var openSuccessModal = function () {
   overlay.classList.add('modal-open');
   successModal.classList.add('modal-open');
   document.addEventListener('keydown', openSuccessModalPressEscHandler);
+  overlay.addEventListener('click', overlayClickHandler);
 };
 var closeSuccessModal = function () {
   overlay.classList.remove('modal-open');
   successModal.classList.remove('modal-open');
   document.removeEventListener('keydown', openSuccessModalPressEscHandler);
+  overlay.removeEventListener('click', overlayClickHandler);
 };
+
 var openSuccessModalPressEscHandler = function (evt) {
   if (evt.keyCode === 27) {
     closeSuccessModal();
@@ -68,14 +76,23 @@ var successModalCloseButtonPressEnterHandler = function (evt) {
   }
 };
 
+var overlayClickHandler = function (evt) {
+  if (callbackModal.classList.contains('modal-open') && evt.target === overlay) {
+    closeCallbackModal();
+  } else if (successModal.classList.contains('modal-open') && evt.target === overlay) {
+    closeCallbackModal();
+  }
+};
+
 callbackOpenButton.addEventListener('click', callbackOpenButtonClickHandler);
 callbackCloseButton.addEventListener('click', callbackCloseButtonClickHandler);
 callbackOpenButton.addEventListener('keydown', callbackOpenButtonPressEnterHandler);
 callbackCloseButton.addEventListener('keydown', callbackCloseButtonPressEnterHandler);
-callbackMeButton.addEventListener('click', function () {
+form.addEventListener('submit', function () {
   closeCallbackModal();
   openSuccessModal();
 });
+//TODO: проблема с отправкой формы, окно с успешным сообщением открывается, но сразу же закрывается.
 
 successModalOKButton.addEventListener('click', closeSuccessModal);
 successModalCloseButton.addEventListener('click', closeSuccessModal);
