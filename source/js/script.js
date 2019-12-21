1eee83895a45320e22ec5b72b84b621a6db039c7
podtes <dpodtes@gmail.com>
2019-12-21 06:20:20 +0400

    реализует логику модальных окон через js

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,83 @@
 'use strict';
 var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var callbackOpenButton = pageHeader.querySelector('.page-header__callback');
+var callbackCloseButton = document.querySelector('.modal-order-callback__close-button');
+var overlay = document.querySelector('.modal-position');
+var callbackModal = overlay.querySelector('.modal-order-callback');
+var callbackMeButton = callbackModal.querySelector('input[type="submit"]');
+var successModal = overlay.querySelector('.modal-success');
+var successModalOKButton = successModal.querySelector('button[type="button"]');
+var successModalCloseButton = successModal.querySelector('.modal-order-callback__close-button');
 
-pageHeader.classList.remove('page-header--nojs');
+var closeCallbackModal = function () {
+  overlay.classList.remove('modal-open');
+  callbackModal.classList.remove('modal-open');
+  document.removeEventListener('keydown', openCallbackModalPressEscHandler);
+};
+var openCallbackModal = function () {
+  overlay.classList.add('modal-open');
+  callbackModal.classList.add('modal-open');
+  document.addEventListener('keydown', openCallbackModalPressEscHandler);
+};
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+var openCallbackModalPressEscHandler = function (evt) {
+  if (evt.keyCode === 27) {
+    closeCallbackModal();
   }
+};
+var callbackOpenButtonClickHandler = function () {
+  openCallbackModal();
+};
+var callbackOpenButtonPressEnterHandler = function (evt) {
+  if (evt.keyCode === 13) {
+    openCallbackModal();
+  }
+};
+var callbackCloseButtonClickHandler = function () {
+  closeCallbackModal();
+};
+var callbackCloseButtonPressEnterHandler = function (evt) {
+  if (evt.keycode === 13) {
+    closeCallbackModal();
+  }
+};
+
+var openSuccessModal = function () {
+  overlay.classList.add('modal-open');
+  successModal.classList.add('modal-open');
+  document.addEventListener('keydown', openSuccessModalPressEscHandler);
+};
+var closeSuccessModal = function () {
+  overlay.classList.remove('modal-open');
+  successModal.classList.remove('modal-open');
+  document.removeEventListener('keydown', openSuccessModalPressEscHandler);
+};
+var openSuccessModalPressEscHandler = function (evt) {
+  if (evt.keyCode === 27) {
+    closeSuccessModal();
+  }
+};
+var successModalOKButtonPressEnterHandler = function (evt) {
+  if (evt.keyCode === 13) {
+    closeSuccessModal();
+  }
+};
+var successModalCloseButtonPressEnterHandler = function (evt) {
+  if (evt.keyCode === 13) {
+    closeSuccessModal();
+  }
+};
+
+callbackOpenButton.addEventListener('click', callbackOpenButtonClickHandler);
+callbackCloseButton.addEventListener('click', callbackCloseButtonClickHandler);
+callbackOpenButton.addEventListener('keydown', callbackOpenButtonPressEnterHandler);
+callbackCloseButton.addEventListener('keydown', callbackCloseButtonPressEnterHandler);
+callbackMeButton.addEventListener('click', function () {
+  closeCallbackModal();
+  openSuccessModal();
 });
+
+successModalOKButton.addEventListener('click', closeSuccessModal);
+successModalCloseButton.addEventListener('click', closeSuccessModal);
+successModalOKButton.addEventListener('keydown', successModalOKButtonPressEnterHandler);
+successModalCloseButton.addEventListener('keydown', successModalCloseButtonPressEnterHandler);
